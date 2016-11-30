e4925137709d98ddc290808b0adec1a5b2e722cc
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-11-30 13:59:16 -0500

    put recipe data in a service

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	// Declare app level module which depends on views, and components
-	angular.module('cookbook', ['ngRoute'])
+	angular.module('cookbook', ['ngRoute', 'RecipeService'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 		.when('/about', {
@@ -25,18 +25,18 @@
 			redirectTo: '/home'
 		})
 	}])
-	.controller('MainController', function($scope) {
-		$scope.recipes = recipes;
+	.controller('MainController', function($scope, RecipeData) {
+		$scope.recipes = RecipeData.recipes;
 	})
 	.controller('AboutController', function($scope) {
 		$scope.message = "Hello!";
 	})
 	.controller('AddRecipeController', function($scope) {
-		
+
 	})
-	.controller('RecipeController', function($scope, $routeParams) {
+	.controller('RecipeController', function($scope, $routeParams, RecipeData) {
 		$scope.recipe_id = $routeParams.recipeID;
-		$scope.recipe = recipes[$routeParams.recipeID];
+		$scope.recipe = RecipeData.recipes[$routeParams.recipeID];
 	})
 	.directive('recipe', function() {
 		return {
@@ -44,86 +44,5 @@
 			templateUrl: 'partials/recipe.html'
 		}
 	});
-
-	var recipes = [
-		{
-			id: 0,
-			title: 'Almond Biscotti',
-			description: 'Crunchy cookies perfect for afternoon tea',
-			ingredients: [
-				{
-					name: 'All-Purpose Flour',
-					quantity: '2 cups'
-				},
-				{
-					name: 'Sugar',
-					quantity: '1 cup'
-				},
-				{
-					name: 'Baking Powder',
-					quantity: '1 teaspoon'
-				},
-				{
-					name: 'Salt',
-					quantity: '1/8 teaspoon'
-				},
-				{
-					name: 'Eggs',
-					quantity: '3'
-				},
-				{
-					name: 'Amaretto',
-					quantity: '2 tablespoons'
-				},
-				{
-					name: 'Vanilla',
-					quantity: '1 teaspoon'
-				},
-				{
-					name: 'Almonds',
-					quantity: '1 cup'
-				}
-			]
-		},
-		{
-			id: 1,
-			title: 'Lemon Bars',
-			description: 'The dessert that launched a thousand ships',
-			ingredients: [
-				{
-					name: 'All-Purpose Flour',
-					quantity: '1 1/2 cups + 3 tablespoons'
-				},
-				{
-					name: 'Confectioners Sugar',
-					quantity: '1/2 cup + 1 tablespoon'
-				},
-				{
-					name: 'Butter',
-					quantity: '3/4 cup'
-				},
-				{
-					name: 'Lemons',
-					quantity: '3'
-				},
-				{
-					name: 'Eggs',
-					quantity: '2'
-				},
-				{
-					name: 'Sugar',
-					quantity: '1 cup'
-				},
-				{
-					name: 'Baking Powder',
-					quantity: '1/2 teaspoon'
-				},
-				{
-					name: 'Salt',
-					quantity: '1/2 teaspoon'
-				}
-			]
-		}
-	];
 
 })();
---
+++ app/recipe.js
@@ -0,0 +1,83 @@
+var RecipeService = angular.module('RecipeService', [])
+.service('RecipeData', function() {
+	this.recipes = [
+		{
+			id: 0,
+			title: 'Almond Biscotti',
+			description: 'Crunchy cookies perfect for afternoon tea',
+			ingredients: [
+				{
+					name: 'All-Purpose Flour',
+					quantity: '2 cups'
+				},
+				{
+					name: 'Sugar',
+					quantity: '1 cup'
+				},
+				{
+					name: 'Baking Powder',
+					quantity: '1 teaspoon'
+				},
+				{
+					name: 'Salt',
+					quantity: '1/8 teaspoon'
+				},
+				{
+					name: 'Eggs',
+					quantity: '3'
+				},
+				{
+					name: 'Amaretto',
+					quantity: '2 tablespoons'
+				},
+				{
+					name: 'Vanilla',
+					quantity: '1 teaspoon'
+				},
+				{
+					name: 'Almonds',
+					quantity: '1 cup'
+				}
+			]
+		},
+		{
+			id: 1,
+			title: 'Lemon Bars',
+			description: 'The dessert that launched a thousand ships',
+			ingredients: [
+				{
+					name: 'All-Purpose Flour',
+					quantity: '1 1/2 cups + 3 tablespoons'
+				},
+				{
+					name: 'Confectioners Sugar',
+					quantity: '1/2 cup + 1 tablespoon'
+				},
+				{
+					name: 'Butter',
+					quantity: '3/4 cup'
+				},
+				{
+					name: 'Lemons',
+					quantity: '3'
+				},
+				{
+					name: 'Eggs',
+					quantity: '2'
+				},
+				{
+					name: 'Sugar',
+					quantity: '1 cup'
+				},
+				{
+					name: 'Baking Powder',
+					quantity: '1/2 teaspoon'
+				},
+				{
+					name: 'Salt',
+					quantity: '1/2 teaspoon'
+				}
+			]
+		}
+	];
+});
